d4f5d36660fad6c696862ee867bf7a17525f00a6
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2023-02-03 07:12:21 +0000

    Added a single test function

---
+++ src/index.test.ts
@@ -0,0 +1,6 @@
+import { test, describe, expect } from '@jest/globals';
+import { strict as assert } from 'node:assert';
+
+test('Write your tests here', () => {
+    assert.ok('Test runs OK');
+});
